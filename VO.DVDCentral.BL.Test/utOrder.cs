f30c987a524496bc3d6f3f58af6129786e5b59c2
Viktoria Odintsova <odinzovaich@gmail.com>
2020-04-29 03:07:09 -0500

    BL tests

--- VO.DVDCentral.BL.Test/utOrder.cs
+++ VO.DVDCentral.BL.Test/utOrder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VO.DVDCentral.BL.Models;
 
 namespace VO.DVDCentral.BL.Test
 {
@@ -7,8 +9,31 @@ namespace VO.DVDCentral.BL.Test
     public class utOrder
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InsertTest()
         {
+            Order order = new Order { UserId = 34, CustomerId = 24, OrderDate = DateTime.Now, ShipDate = DateTime.Now };
+            Assert.AreNotEqual(0, OrderManager.Insert(order));
+        }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            List<Order> orders = OrderManager.Load();
+            Assert.AreEqual(3, orders.Count);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            Order order = OrderManager.LoadById(4);
+            order.UserId = 555;
+            Assert.IsTrue(OrderManager.Update(order) > 0);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            Assert.IsTrue(OrderManager.Delete(4) > 0);
         }
     }
 }
---
+++ VO.DVDCentral.BL.Test/utOrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VO.DVDCentral.BL.Models;
+
+namespace VO.DVDCentral.BL.Test
+{
+    [TestClass]
+    public class utOrderItem
+    {
+        [TestMethod]
+        public void LoadByOrderIdTest()
+        {
+            List<OrderItem> items = OrderItemManager.LoadByOrderId(2);
+            Assert.AreEqual(1, items.Count);
+        }
+    }
+}
